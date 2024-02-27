e50fa67acaff22558c2ba7818a143fda67c0f5e1
Pallinger Péter <pallinger@sztaki.hu>
2024-02-27 15:20:56 +0100

    generate_password_hash disabled all other passwords definable

--- files/generate_password_hash_and_salt.py
+++ files/generate_password_hash_and_salt.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
+
+import os, sys
 import hashlib
 import base64
-import sys
 from hashlib import pbkdf2_hmac
+
+# Generate a new 16-byte salt
+new_salt = os.urandom(16)
+decoded_salt = new_salt  # Use this in the pbkdf2_hmac function
 
 # Your new password
 new_password = sys.argv[1]
-
-# Salt - you can generate a new salt for each password
-# Here, I'm using the same salt as in your previous example for consistency
-salt = sys.argv[2]
-decoded_salt = base64.b64decode(salt)
 
 # Hashing parameters
 hash_iterations = 27500
@@ -19,7 +20,9 @@ algorithm = 'sha256'
 new_hash = pbkdf2_hmac(algorithm, new_password.encode(), decoded_salt, hash_iterations)
 
 # Encoding the generated hash to a format similar to your secret data value
+encoded_new_salt = base64.b64encode(new_salt).decode()
 encoded_new_hash = base64.b64encode(new_hash).decode()
 
 # The encoded hash
+print(encoded_new_salt)
 print(encoded_new_hash)
